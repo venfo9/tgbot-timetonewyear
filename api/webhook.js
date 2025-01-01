ebe5e4243acd91978aa40739b6bd650f520faea2
B <59208263+venfo9@users.noreply.github.com>
2025-01-01 22:38:42 +0200

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,11 @@
 const { Telegraf } = require('telegraf');
-const { DateTime } = require('luxon');
+const { DateTime } = require('luxon'); // Для удобной работы с датами
 const process = require('process');
 
 // Получаем токен из переменных окружения
 const TOKEN = process.env.BOT_TOKEN;
 const bot = new Telegraf(TOKEN);
 
-// Обработчик команды /start
 const start = async (ctx) => {
   await ctx.reply(
     'Привет! Напиши /time, чтобы узнать, сколько дней, минут и секунд осталось до Нового года.\n' +
@@ -14,7 +13,6 @@ const start = async (ctx) => {
   );
 };
 
-// Обработчик команды /time
 const timeToNewYear = async (ctx) => {
   const now = DateTime.local();
   const newYear = DateTime.local().plus({ years: 1 }).startOf('year');
@@ -32,7 +30,7 @@ const timeToNewYear = async (ctx) => {
 bot.command('start', start);
 bot.command('time', timeToNewYear);
 
-// Это будет файл, который Vercel будет использовать для обработки запросов на webhook.
+// Сервер для работы с вебхуками Vercel
 module.exports = async (req, res) => {
   if (req.method === 'POST') {
     try {
@@ -44,7 +42,6 @@ module.exports = async (req, res) => {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   } else {
-    console.log('Method not allowed: ', req.method);
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
 };
